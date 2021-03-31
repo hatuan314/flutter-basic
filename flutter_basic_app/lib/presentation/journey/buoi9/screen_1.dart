3d85f76588b40fb7ced6b3f1a58bd7dd141ba060
cloudmadeofcandy <cloudmadeofcandy@gmail.com>
2021-03-31 15:10:59 +0700

    Merge remote-tracking branch 'origin/TQQ_2' into TQQ_2
    # Conflicts:
    #	flutter_basic_app/lib/presentation/journey/buoi6/screen_2.dart

--- flutter_basic_app/lib/presentation/journey/buoi9/screen_1.dart
+++ flutter_basic_app/lib/presentation/journey/buoi9/screen_1.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class B9Screen2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
     return Scaffold(
 
     );
