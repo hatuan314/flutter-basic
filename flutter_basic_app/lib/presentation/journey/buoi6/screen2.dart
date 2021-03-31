a7319651e5002b7444ed0fde4c35f744dc13d44f
cloudmadeofcandy <cloudmadeofcandy@gmail.com>
2021-03-31 15:14:42 +0700

    Mar 31, 2021

--- flutter_basic_app/lib/presentation/journey/buoi6/screen2.dart
+++ flutter_basic_app/lib/presentation/journey/buoi6/screen2.dart
@@ -42,32 +42,124 @@ class B6Screen2 extends StatelessWidget {
               backgroundImage: AssetImage("assets/images/1.jpg"),
             )),
       ],
-      title:
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Zomo Food',
-              style: TextStyle(
-                color: Colors.black,
-              ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Zomo Food',
+            style: TextStyle(
+              color: Colors.black,
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 
   Widget _body() {
+    var _controller = TextEditingController();
     return Scaffold(
       backgroundColor: Colors.white10,
-      body:
-      Column(
+      body: Column(
         children: [
+          Container(
+            // height: ,
+            padding: EdgeInsets.all(5.0),
+            child: Stack(
+              children: [
+                TextField(
+                  style: TextStyle(
+                    // fontSize: 20
+                  ),
+                  decoration: InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.fromLTRB(15, 20, 110, 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(35),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
+                ),
+                Positioned(
+                  top: 5,
+                  right: 10,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0)),
+                    child: (RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Filter  ",
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                          WidgetSpan(
+                            child: Icon(
+                              Icons.ballot,
+                              size: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+                    onPressed: null,
+                    disabledColor: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ), //Searchbar
+
+          Container(
+            height: 140,
+            child: ListView(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                _itemWidget("hello","assets/images/1.jpg", null),
+                _itemWidget("hi","assets/images/1.jpg",  null),
+                _itemWidget("hello","assets/images/1.jpg", null),
+                _itemWidget("hi","assets/images/1.jpg", null),
+                _itemWidget("hello","assets/images/1.jpg", null),
+                _itemWidget("hi","assets/images/1.jpg", null),
+                _itemWidget("hello","assets/images/1.jpg", null),
+                _itemWidget("hi","assets/images/1.jpg", null),
+              ],
+            ),
+          ),
+
           Padding(
             padding: EdgeInsets.zero,
           ),
         ],
       ),
+    );
+  }
+
+  Widget _itemWidget(String title, String photos, Function onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+            border: Border.all(color: Colors.grey)),
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage(photos),
+            ),
+            SizedBox(height: 15,),
+            Text(title),
+          ],
+        ),
+      ),
+
     );
   }
 
