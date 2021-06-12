13c5d0ab42a1b7e202e1f7073746b5184dd40d52
tuanha <tuanha@acaziasoft.com>
2021-06-12 10:15:03 +0700

    push code

---
+++ ghichu/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/tuanha/Setup-flutter/flutter-2.0.4"
+export "FLUTTER_APPLICATION_PATH=/Users/tuanha/Work/KIT/Flutter/flutter-basic/ghichu"
+export "FLUTTER_TARGET=/Users/tuanha/Work/KIT/Flutter/flutter-basic/ghichu/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/tuanha/Work/KIT/Flutter/flutter-basic/ghichu/.dart_tool/package_config.json"
